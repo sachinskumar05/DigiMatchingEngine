73d16d73ec70074a5842e376aa4a8f80995382b4
SK <sachin.skumar05@gmail.com>
2021-02-01 16:44:42 +0800

    One more test case.. Once get more time to test/modify will add some more cases

--- src/test/java/com/digi/matching/engine/FXMatchingEngineTest.java
+++ src/test/java/com/digi/matching/engine/FXMatchingEngineTest.java
@@ -4,8 +4,7 @@ import com.digi.matching.client.FXClientBuy;
 import com.digi.matching.client.FXClientSell;
 import com.digi.matching.exchange.order.FXOrder;
 import com.digi.matching.exchange.order.Trade;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +14,7 @@ import java.util.concurrent.Executors;
 import static com.digi.matching.types.OrderType.LIMIT;
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FXMatchingEngineTest {
 
 
@@ -26,15 +26,20 @@ class FXMatchingEngineTest {
     @BeforeAll
     public static void init(){
         fxMatchingEngine = FXMatchingEngine.getInstance();
-        fxClientSell = new FXClientSell();
-        fxClientBuy = new FXClientBuy();
         executorService = Executors.newFixedThreadPool(5);
     }
+
+    @BeforeEach
+    public void clientInit(){
+        fxClientSell = new FXClientSell();
+        fxClientBuy = new FXClientBuy();
+    }
     @Test
+    @Order(1)
     public void testSimpleBuyAndSell() throws InterruptedException {
         fxClientSell.createAndSubmitOrder(1, 20000.0d, 10, LIMIT);
         fxClientBuy.createAndSubmitOrder(1, 20000.0d, 10, LIMIT);
-        Thread.sleep(100);
+        Thread.sleep(50);
 
         List<FXOrder> fxClientSellOrdList = fxClientSell.getClientOrders();
         List<FXOrder> fxClientBuyOrdList  = fxClientBuy.getClientOrders();
@@ -56,6 +61,31 @@ class FXMatchingEngineTest {
         assertEquals(sellTrades.get(0).tradeQty, sellFXOrder.getOrdQty()-sellFXOrder.getLeavesQty());
         assertEquals(sellTrades.get(0).tradeQty, buyFXOrder.getOrdQty()-buyFXOrder.getLeavesQty());
         assertEquals(buyTrades.get(0).tradeQty, sellTrades.get(0).tradeQty);
+    }
+
+    @Test
+    @Order(2)
+    public void testSimpleBuyLowAndSellHigh() throws InterruptedException {
+
+        Thread.sleep(50);
+        fxClientSell.createAndSubmitOrder(1, 20000.0d, 10, LIMIT);
+        fxClientBuy.createAndSubmitOrder(1, 19000.0d, 10, LIMIT);
+        List<FXOrder> fxClientSellOrdList = fxClientSell.getClientOrders();
+        List<FXOrder> fxClientBuyOrdList  = fxClientBuy.getClientOrders();
+        Thread.sleep(50);
+
+        FXOrder sellFXOrder =  fxClientSellOrdList.get(0);
+        FXOrder buyFXOrder =  fxClientBuyOrdList.get(0);
+
+        System.out.println(sellFXOrder);
+        System.out.println(buyFXOrder);
+
+        assertEquals(sellFXOrder.getTradeCount(), 0);
+        assertEquals(buyFXOrder.getTradeCount(), 0);
+
+        Thread.sleep(50);
+        fxClientSell.createAndSubmitOrder(1, 19000.0d, 10, LIMIT);//Clearing book
+        fxClientBuy.createAndSubmitOrder(1, 20000.0d, 10, LIMIT);//Clearing book
 
     }
 }
